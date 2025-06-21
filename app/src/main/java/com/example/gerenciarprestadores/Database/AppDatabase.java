a785da789bf831c3adea28fea33ba9d5294eba97
Emanuelle Machado <manumachado03@outlook.com>
2025-06-20 21:03:41 -0300

    correção do converters

--- app/src/main/java/com/example/gerenciarprestadores/Database/AppDatabase.java
+++ app/src/main/java/com/example/gerenciarprestadores/Database/AppDatabase.java
@@ -43,14 +43,3 @@ public abstract class AppDatabase extends RoomDatabase {
     }
 }
 
-class Converters {
-    @TypeConverter
-    public static Date fromTimestamp(Long value) {
-        return value == null ? null : new Date(value);
-    }
-
-    @TypeConverter
-    public static Long dateToTimestamp(Date date) {
-        return date == null ? null : date.getTime();
-    }
-}
